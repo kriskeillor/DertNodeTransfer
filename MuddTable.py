13cd170637d3aff0e7c7c30a597ed2c58feb1669
keillok <keillok@wwu.edu>
2022-06-05 15:42:13 -0700

    Add append function and full sample test data

--- MuddTable.py
+++ MuddTable.py
@@ -23,7 +23,7 @@ except ImportError:
     sys.exit(-1)
 # Modules
 try:
-    import datetime
+    from datetime import datetime
 except ImportError:
     print("Datetime module import failed")
     sys.exit(ERR_GENERIC)
@@ -41,16 +41,31 @@ except ImportError:
 # Bulk add-value to all rows
 def append_values_bulk(fname_in, col_add, fname_out):
     data_out = []
+    # Open input
     filestream_in = open_reader_stream(fname_in)
     with filestream_in:
         reader = csv.reader(filestream_in)
         headers = next(reader)
         data_out = [headers] + [row + [col_add] for i, row in enumerate(reader)]
+    filestream_in.close()
+    # Open output
     filestream_out = open_writer_stream(fname_out)
     with filestream_out:
         csv.writer(filestream_out, delimiter=",").writerows(data_out)
+        filestream_out.close()
+    filestream_out.close()
 
     return ERR_NONE
+
+# Append a single name-value pair, automatically generating timestamp
+def append_entry(fname_in, val_name, value):
+    data_out = []
+    # Open input
+    filestream_in = open_append_stream(fname_in)
+    with filestream_in:
+        writer = csv.writer(filestream_in)
+        writer.writerow([val_name, value, datetime.now()])
+    filestream_in.close()
 
 
 #   *   *   *   *   *   *
@@ -59,7 +74,7 @@ def append_values_bulk(fname_in, col_add, fname_out):
 # Read object
 def open_reader_stream(fname):
     try:
-        reader_str = open(fname, "rt", newline='')   # Open in "read text" mode, universal newline
+        reader_str = open(fname, "rt", newline='')   # Open in "read text" mode
         return reader_str
     except:
         print("Could not read file " + fname)
@@ -67,9 +82,32 @@ def open_reader_stream(fname):
 # Write object
 def open_writer_stream(fname):
     try:
-        writer_str = open(fname, "wt", newline='')   # Open in "write text" mode, universal newline
+        writer_str = open(fname, "wt", newline='\n')   # Open in "write text" mode
         return writer_str
     except:
         print(fname)
         print("Could not write file {}".format(fname))
         sys.exit(ERR_NO_DATA)
+# Append object
+def open_append_stream(fname):
+    try:
+        append_str = open(fname, "at", newline='\n')   # Open in "append text" mode
+        return append_str
+    except:
+        print(fname)
+        print("Could not append file {}".format(fname))
+        sys.exit(ERR_NO_DATA)
+# Print a CSV table
+def print_stream(fname):
+    #reader_str
+    try:
+        reader_str = open(fname, "rt", newline='\n')
+    except:
+        print("Could not open file {}".format(fname))
+        return ERR_NO_DATA
+    with (reader_str):
+        row = next(reader_str, None)
+        while row is not None:
+            print(row)
+            row = next(reader_str, None)
+    return ERR_NONE
